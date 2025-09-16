2b9c02f96e125b1e85efe129f8e3bf4347732ee7
Chunsig-i <junsumon090608@dgsw.hs.kr>
2025-09-16 21:33:12 +0900

    add entire client project

---
+++ client/src/App.tsx
@@ -0,0 +1,20 @@
+import {Routes,Route} from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+
+export default function App() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-1">
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path="/signup" element={<Signup />}/>
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  )
+}
