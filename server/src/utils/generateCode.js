6e5adfc9a3765618b34d8dd9f0290de2a712eba7
fita8547 <junsumon090608@gmail.com>
2025-11-17 22:09:01 +0900

    wow

--- server/src/utils/generateCode.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateNumbericCode = generateNumbericCode;
-function generateNumbericCode(len = 6) {
-    let s = "";
-    for (let i = 0; i < len; i++)
-        s += Math.floor(Math.random() * 10);
-    return s;
-}
